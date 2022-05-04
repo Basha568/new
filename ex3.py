a4f31a7838251148b905ac16a2ee4eef344da6f6
kareembasha123 <kareembasha354@gmail.com>
2022-05-04 20:18:23 +0530

    first commit

---
+++ ex3.py
@@ -0,0 +1,4 @@
+days="Mon Tue Wed Thr Fri Sat Sun"
+months="jan\n feb \n mar \n apr \n jun \n jul \n sep \n oct \n nov\n dec"
+print("Here are these days:",days)
+print("Here are these months:",months)
---
+++ ex6.py
@@ -0,0 +1,4 @@
+age=input("How old are you?")
+height=input("how much tall are you?")
+weight=input("how much weight are you?")
+prrint(f"So, you're {age} old, {height} tall and {weight} heavy.")
